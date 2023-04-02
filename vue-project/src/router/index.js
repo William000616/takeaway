77843c5214f5d36712634ea95e164b319700815e
William <huangjixiang0616@163.com>
2023-04-02 16:25:26 +0800

    修复路由

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -278,20 +278,21 @@ const router = new VueRouter({
   routes
 });
 
-router.beforeEach((to, from, next) => {
-  // document.title = `${to.meta.title} | 饱了吗外卖后台管理系统`;
-  document.title = `饱了吗外卖`;
-  console.log(to.path)
-  if (to.path !== '/login') {
-    next('/login');
-  } else if (to.meta.permission) {
-    // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
-    role === 'admin' ?
-      next() :
-      next('/403');
-  } else {
-    next();
-  }
-});
+// router.beforeEach((to, from, next) => {
+//   // document.title = `${to.meta.title} | 饱了吗外卖后台管理系统`;
+//   document.title = `饱了吗外卖`;
+//   console.log(to.path)
+//   const role = localStorage.getItem('ms_username');
+//   if (to.path !== '/login') {
+//     next('/login');
+//   } else if (to.meta.permission) {
+//     // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
+//     role === 'admin' ?
+//       next() :
+//       next('/403');
+//   } else {
+//     next();
+//   }
+// });
 
 export default router
